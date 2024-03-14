71c2c47df6f70ffa515dce718bf573ce2b5df430
tamanna1321 <159517381+tamanna1321@users.noreply.github.com>
2024-03-14 16:43:18 +0530

    class with name.java

---
+++ Tamanna.java
@@ -0,0 +1,11 @@
+class Tamanna{
+    public static void main(String args[])
+    {
+        System.out.println("My name is Tamanna");
+        System.out.println("I am 18 years old");
+        System.out.println("I have keen interest in coding");
+        System.out.println("Being a melophile,In my leisure time I love to do singing");
+        System.out.println("I belong to a renowned city of Punjab,Jalandhar");
+    }
+    
+}
