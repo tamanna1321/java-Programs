d85af630fca764027e2bbea1dd68a5d64a89fdf3
tamanna1321 <159517381+tamanna1321@users.noreply.github.com>
2024-03-14 16:47:13 +0530

    Static/non static.java

---
+++ Calcirunner.java
@@ -0,0 +1,19 @@
+class Calci{
+    
+public static void sum(){
+    System.out.println("welcome to sum");
+}
+public void sub(){
+
+System.out.println("welcome to  sub");
+}
+}
+public class Calcirunner
+{
+    public static void main(String a[])
+    {
+        Calci.sum();
+        Calci aa=new Calci();
+        aa.sub();
+    }
+}
