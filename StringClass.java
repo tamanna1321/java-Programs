5ce2c33c054716d5c2af05c08aa6559aa34024e8
tamanna1321 <159517381+tamanna1321@users.noreply.github.com>
2024-03-26 21:00:28 +0530

    Stringclass.java

---
+++ StringClass.java
@@ -0,0 +1,14 @@
+public class StringClass
+{
+    public static void main(String args[])
+{
+      String OriginalString  = "Java Excersises!";
+    
+    int index1 = 0;
+    int index2 =10;
+   System.out.println(" OriginalString="+OriginalString);
+   System.out.println("the character at position="+index1+ "is" +OriginalString.charAt(index1));
+   System.out.println("the character at position="+index2+ "is" +OriginalString. charAt(index2));
+   
+}
+}
