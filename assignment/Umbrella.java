2272de57d01f9fcfd19f340b62f7918558657911
tamanna1321 <159517381+tamanna1321@users.noreply.github.com>
2024-03-26 21:07:13 +0530

    umbrella.java

---
+++ assignment/Umbrella.java
@@ -0,0 +1,15 @@
+class Umbrella{
+    public static void main(String args[])
+    {
+
+
+String word="umbrella";
+if(word.contains ("e")){
+System.out.println("the word has letter e");
+}
+else
+   { System.out.println("the word has no letter e");
+ }
+}
+}
+
