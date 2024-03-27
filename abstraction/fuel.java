5c2b50005f0e542f416267969492ba1a84bba818
tamanna1321 <159517381+tamanna1321@users.noreply.github.com>
2024-03-27 21:37:49 +0530

    fuel.java

---
+++ abstraction/fuel.java
@@ -0,0 +1,25 @@
+package abstraction;
+interface moveable{
+    void move();
+}
+
+interface speedable{
+    void speed(int speeding);
+}
+
+class Vehicle implements moveable,speedable{
+    public void move(){
+        System.out.println("Moving the vehicle");
+    }
+    public void speed(int speeding){
+        System.out.println("speeding the vehicle");
+    }
+    }
+    public class fuel{
+        public static void main(String[]args){
+            Vehicle aa= new Vehicle();
+            aa.move();
+            aa.speed(5);
+
+        }
+    }
